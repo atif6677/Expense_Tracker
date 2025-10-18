649bed4eff610c52ec86a727c0040cc91e504e04
Mohd Atif <mohd.atif2531@gmail.com>
2025-10-18 22:34:43 +0530

    fix database

--- src/utils/database.js
+++ src/utils/database.js
@@ -4,9 +4,10 @@ const { Sequelize } = require('sequelize');
 
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql'
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql'
 });
+
 
 
 (async ()=>{
